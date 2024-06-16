b37a5ad947b28a395bf0b3a56e0f61f4ad5818a9
yacine <a_zeraibi@estin.dz>
2024-06-16 18:34:29 +0100

    init migration

---
+++ Server/prisma/migrations/20240616173420_init/migration.sql
@@ -0,0 +1,109 @@
+-- CreateEnum
+CREATE TYPE "Gender" AS ENUM ('Male', 'Female');
+
+-- CreateEnum
+CREATE TYPE "UserRole" AS ENUM ('Student', 'Professor');
+
+-- CreateEnum
+CREATE TYPE "CoursworkType" AS ENUM ('Assignment', 'Quiz', 'Project', 'Exam', 'Other');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "gender" "Gender" NOT NULL,
+    "role" "UserRole" NOT NULL,
+    "image" TEXT,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Classroom" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "professorId" TEXT NOT NULL,
+
+    CONSTRAINT "Classroom_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Enrollment" (
+    "id" TEXT NOT NULL,
+    "membreId" TEXT NOT NULL,
+    "classroomId" TEXT NOT NULL,
+
+    CONSTRAINT "Enrollment_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Courswork" (
+    "id" TEXT NOT NULL,
+    "classroomId" TEXT NOT NULL,
+    "professorId" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "type" "CoursworkType" NOT NULL,
+    "description" TEXT NOT NULL,
+    "due_date" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Courswork_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Submission" (
+    "id" TEXT NOT NULL,
+    "CoursworkId" TEXT NOT NULL,
+    "studentId" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+
+    CONSTRAINT "Submission_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "File" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "path" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "size" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "submissionId" TEXT,
+    "coursworkId" TEXT,
+
+    CONSTRAINT "File_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Classroom" ADD CONSTRAINT "Classroom_professorId_fkey" FOREIGN KEY ("professorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Enrollment" ADD CONSTRAINT "Enrollment_membreId_fkey" FOREIGN KEY ("membreId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Enrollment" ADD CONSTRAINT "Enrollment_classroomId_fkey" FOREIGN KEY ("classroomId") REFERENCES "Classroom"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Courswork" ADD CONSTRAINT "Courswork_classroomId_fkey" FOREIGN KEY ("classroomId") REFERENCES "Classroom"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Courswork" ADD CONSTRAINT "Courswork_professorId_fkey" FOREIGN KEY ("professorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Submission" ADD CONSTRAINT "Submission_CoursworkId_fkey" FOREIGN KEY ("CoursworkId") REFERENCES "Courswork"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Submission" ADD CONSTRAINT "Submission_studentId_fkey" FOREIGN KEY ("studentId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "File" ADD CONSTRAINT "File_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "File" ADD CONSTRAINT "File_submissionId_fkey" FOREIGN KEY ("submissionId") REFERENCES "Submission"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "File" ADD CONSTRAINT "File_coursworkId_fkey" FOREIGN KEY ("coursworkId") REFERENCES "Courswork"("id") ON DELETE SET NULL ON UPDATE CASCADE;
